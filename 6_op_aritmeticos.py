b0f06016ed40624eb6c0516648cf33b8e12523fa
hy <henrryyanez@gmail.com>
2018-12-11 23:14:29 -0300

    Create 6_op_aritmeticos.py
    Pruebas con operadores aritméticos.

---
+++ 6_op_aritmeticos.py
@@ -0,0 +1,25 @@
+#Operadores aritmeticos con variables - Sesión 6
+#Parentesis ()
+#Exponenciacion **
+#Multiplicación, Division y Modulo: *, /, %
+#Suma y resta + -
+
+num1 = 10
+num2 = 3
+
+resultado = num1 + num2
+resultado2 = num1 - num2
+resultado3 = num1 / num2   #Divisiones
+resultado4 = num1 // num2  #Redondeo a la baja
+resultado5 = num1 % num2   # Calculo de modulo, la division de 10 entre 3 va a mostrar el residuo
+resultado6 = num1 ** num2  #Crear un exponente es decir num1 elevado a la num2
+resultado7 = 3**3 * (13/5 - (2*4))  #Recordemos que los parentesis se resuelven primero
+
+
+print(resultado)
+print(resultado2)
+print(resultado3)
+print(resultado4)
+print(resultado5)
+print(resultado6)
+print(resultado7)
