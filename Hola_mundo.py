e73ba4201f50333773680edfc9157c160ae3fbdd
hy <henrryyanez@gmail.com>
2018-12-11 23:01:05 -0300

    Inicio de practicas
    Comprobando variables.

---
+++ Hola_mundo.py
@@ -0,0 +1,20 @@
+numero = 10
+numero2 = 10.45
+cadena = 'a'
+cadena2 = 'Estoy "estudiando"'
+valor = True
+
+
+print(numero)
+print(type(numero))
+
+print(numero2)
+print(type(numero2))
+
+print(cadena)
+print(type(cadena))
+
+print(cadena2)
+
+print(valor)
+print(type(valor))
