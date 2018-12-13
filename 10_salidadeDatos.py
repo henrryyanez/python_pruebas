521fabe1def4eeab32b59f3d10b5f94327be6984
hy <henrryyanez@gmail.com>
2018-12-12 22:53:52 -0300

    Pruebas con salidas de datos y concatenación
    Pruebas de concatenación y salida de datos

---
+++ 10_salidadeDatos.py
@@ -0,0 +1,10 @@
+#Salida de Datos
+
+nombre = "Jack"
+edad = 21
+
+print("Hola",nombre,"tienes",edad,"años")
+
+print("Hola {} tienes {} años".format(nombre,edad))
+
+print(f"Hola {nombre} tienes {edad} años")  #Nueva forma que python tiene para la salida por consola
