c49fe53d1cd16ba9a3f9056212b73afd2fbfba9a
hy <henrryyanez@gmail.com>
2018-12-12 23:46:32 -0300

    Operando con Entrada de datos
    Entradas de datos.

---
+++ 11_entradaDatos.py
@@ -0,0 +1,12 @@
+#Entrada de Datos
+
+#Ahora ingresamos valores tipo  string
+nombre = input("Digite su nombre: ")
+
+print(f"Hola {nombre}")
+
+#Ahora ingresamos valores tipo enteros
+
+numero = int(input("Digite el número: "))
+
+print(f"El número es {numero+1}")
