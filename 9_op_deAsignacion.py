9966c0dc1aa45970e56fc085d25b6c78bd4de3c8
hy <henrryyanez@gmail.com>
2018-12-12 22:19:43 -0300

    Create 9_op_deAsignacion.py
    Pruebas con operadores de asignación

---
+++ 9_op_deAsignacion.py
@@ -0,0 +1,12 @@
+#Operadores de Asignación, aumentamos un determinado valor
+
+a = 0
+
+a += 5    #Suma en asignación
+a -= 2    #Resta en asignación
+a *= 3    #Multiplicación en asignación
+a /= 3    #División en asignación
+a **=2    #Potencia en asignación, elevamos al cuadrado
+a %=2     #Modullo en asignación, residuo de la división
+
+print(a)
