59301a4d684cee235153c1a691cfbd7502bdfb26
hy <henrryyanez@gmail.com>
2018-12-16 20:08:08 -0300

    Update usodeListas.py

--- usodeListas.py
+++ usodeListas.py
@@ -5,4 +5,5 @@ print(bicycles)
 
 #Accediendo a elementos en una lista
 print(bicycles[2])
+print(bicycles[-1])  #Se devuelve en la eleccion del elemento de derecha a izquierda
 print(bicycles[1].title())
