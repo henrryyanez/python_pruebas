f03cb9eea54215e078543f6dc1f09cdf01e67f02
hy <henrryyanez@gmail.com>
2018-12-16 19:36:10 -0300

    Espacios en blanco

---
+++ whitespace..py
@@ -0,0 +1,7 @@
+#Stripping Whitespace, Espacions en blanco, con el metodo rstrip()
+
+favorite_language = 'python '
+
+print(favorite_language)
+print(favorite_language.rstrip()) #Eliminamos el espacio del final del string
+
