ee4f03a2f7ff66aacf9bca7bdfe44313704e9416
hy <henrryyanez@gmail.com>
2018-12-15 21:13:39 -0300

    Valores absolutos

--- 12-funcionesIntegradas.py
+++ 12-funcionesIntegradas.py
@@ -6,10 +6,14 @@ n2 = hex(12)  #Convertir un entero a hexadecimal
 n3 = int(0b1011)  #Convertir un binario a un numero entero
 n4 = int("0b1011",2) #le decimos que es base 2
 n5 = int("0xa",16)   #Indicamos que como es hexadec es base 16
+n6 = abs(-49)   #Valor absoluto
+n7 = round(10.56)  #Redondeo de numero
 
-print(n, n1, n2, n3, n4, n5)
+print(n, n1, n2, n3, n4, n5, n6, n7)
 print(n1)
 print(n2)
 print(n3)
 print(n4)
 print(n5)
+print(n6)
+print(n7)
