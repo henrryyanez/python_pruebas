da5a061236cc5cd0f3bfd571e269bebc1bc0e598
hy <henrryyanez@gmail.com>
2018-12-16 19:22:14 -0300

    Usando Metodos y aplicando Concatenación
    Usando Metodos y aplicando Concatenación

--- metodo.py
+++ metodo.py
@@ -1,6 +1,15 @@
+#Usando Metodos y aplicando Concatenación
 
-name = "ada lovelace"
+name = "carla blanco"
 
-print(name.title())   #Usando el metodo "title", para que la salida muestre tipo titulo
+first_name = "ada"
+last_name = "coronado"
+full_name = first_name + " " + last_name
+
+
+print(name.title())   #Usando el metodo "name" y la funcion "title", para que la salida muestre tipo titulo
 print(name.upper())
 print(name.lower())
+
+print(full_name)
+print("Hola, " + full_name.title() + "!")
