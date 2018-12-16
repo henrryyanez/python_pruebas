c9b465df2cd741f7e7f565e671eec692a87aefc9
hy <henrryyanez@gmail.com>
2018-12-16 19:28:42 -0300

    Uso de tabs y nuevas lineas
    Practicas con Tabs y Lineas nuevas

---
+++ tabs_libro.py
@@ -0,0 +1,9 @@
+#Uso de tabs o nuevas lineas para hacer escapes
+# \t  Agregando Tabs
+# \n  Agregando nueva linea siguiente
+
+print("\tPython")
+print("\n")
+print("Lenguaje:\nPython\nC\nJavaScript")
+print("\n")
+print("Lenguaje:\n\tPython\n\tC\n\tJavaScript")
