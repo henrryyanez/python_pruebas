f06455a417f1bb195e98dc521ead2f53f45a38ce
hy <henrryyanez@gmail.com>
2018-12-16 19:16:49 -0300

    Update metodo.py

--- metodo.py
+++ metodo.py
@@ -1,4 +1,4 @@
 
 name = "ada lovelace"
 
-print(name.title())   #Usando el metodo "name" y la funcion "title", para que la salida muestre tipo titulo
+print(name.title())   #Usando el metodo "title", para que la salida muestre tipo titulo
