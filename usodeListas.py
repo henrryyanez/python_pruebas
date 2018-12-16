84f00610f33d26a97bb375cedd266e3a754a991e
hy <henrryyanez@gmail.com>
2018-12-16 20:06:39 -0300

    Agregamos y operamos con Listas
    Operado con Listas  seleccionando valores de la cadena y aplicandole metodos

---
+++ usodeListas.py
@@ -0,0 +1,8 @@
+#Uso de Listas LIST
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+#Accediendo a elementos en una lista
+print(bicycles[2])
+print(bicycles[1].title())
