f6d6ebceacd9d2466afce84cc09c4278faca380a
hy <henrryyanez@gmail.com>
2018-12-16 19:38:57 -0300

    Update whitespace..py

--- whitespace..py
+++ whitespace..py
@@ -3,5 +3,7 @@
 favorite_language = 'python '
 
 print(favorite_language)
-print(favorite_language.rstrip()) #Eliminamos el espacio del final del string
+print(favorite_language.rstrip()) #Quita espacio en blanco de la derecha
+print(favorite_language.lstrip()) #Quita espacio en blanco de la izquierda
+print(favorite_language.strip())  #Quita espacio en blanco de los 2 lados
 
