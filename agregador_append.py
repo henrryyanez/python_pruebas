1cfa34de3a37d618ced4c979065aadb425a4a510
hy <henrryyanez@gmail.com>
2018-12-16 20:20:18 -0300

    Usando el append para agregar datos a una variable

---
+++ agregador_append.py
@@ -0,0 +1,14 @@
+#Uso de agregador "append"
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles.append('ducati')
+print(motorcycles)
+
+#########Agregamos datos a "moto" con append###############
+moto = []
+moto.append('motohonda')
+moto.append('motoyamaha')
+moto.append('motosuzuki')
+print(moto)
