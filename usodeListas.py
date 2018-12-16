bb1fc67b015ebd1aacaf009586b732a1403dad28
hy <henrryyanez@gmail.com>
2018-12-16 20:10:27 -0300

    Update usodeListas.py

--- usodeListas.py
+++ usodeListas.py
@@ -1,9 +1,13 @@
 #Uso de Listas LIST
 
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message = "Mi primera bici fue una " + bicycles[0].title() + "."
+
 print(bicycles)
 
 #Accediendo a elementos en una lista
 print(bicycles[2])
 print(bicycles[-1])  #Se devuelve en la eleccion del elemento de derecha a izquierda
 print(bicycles[1].title())
+
+print(message)
