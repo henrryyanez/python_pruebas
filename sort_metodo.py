a34603d74888faacac44140ca8e7f5bfd130a269
hy <henrryyanez@gmail.com>
2018-12-17 10:04:59 -0300

    aplicando sort

---
+++ sort_metodo.py
@@ -0,0 +1,16 @@
+#Uso del metodo Sort para ordenar
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+
+###Orden reverso
+cars.sort(reverse=True)
+print(cars)
+
+####
+print('')
+print("Aqui esta la lista original:")
+print(cars)
+print("Aqui aplicamos el metodo:")
+print(sorted(cars))
