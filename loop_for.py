b6e61374711ac8970c67bdf6f2ba5dd074c51198
hy <henrryyanez@gmail.com>
2018-12-18 13:08:29 -0300

    Uso del for para generar una lista con los valores al cuadrado

--- loop_for.py
+++ loop_for.py
@@ -21,3 +21,11 @@ print(numbers)
 print("\nOtro ejemplo 3:\n")
 even_numbers = list(range(2,11,2))  #Aumenta de 2 en 2 sin pasarse de 11 en el loop
 print(even_numbers)
+
+print("\nOtro ejemplo 4:\n")
+
+squares = []
+for value in range(1,11):
+    square = value**2
+    squares.append(square)
+print(squares)
