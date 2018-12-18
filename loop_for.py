a91e8bb68d0d71dc4d2051c816e95de04e0cbb82
hy <henrryyanez@gmail.com>
2018-12-18 13:14:37 -0300

    Loop con lista de resultados

--- loop_for.py
+++ loop_for.py
@@ -36,3 +36,7 @@ digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 print(min(digits))
 print(max(digits))
 print(sum(digits))
+
+print("\nAplicando metodos al resutado 6:\n")
+squares = [value**2 for value in range(1,11)]
+print(squares)
