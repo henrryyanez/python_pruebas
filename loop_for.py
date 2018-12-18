7f8230abb8f5b5e4fa41910b17d50f9d65c231fe
hy <henrryyanez@gmail.com>
2018-12-18 13:13:00 -0300

    Usando Maximos, minimos y sumatorias en resultados

--- loop_for.py
+++ loop_for.py
@@ -29,3 +29,10 @@ for value in range(1,11):
     square = value**2
     squares.append(square)
 print(squares)
+
+print("\nAplicando metodos al resutado 5:\n")
+
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(min(digits))
+print(max(digits))
+print(sum(digits))
