bfded5aa981f216ff6578f11f14e8447c288fa38
hy <henrryyanez@gmail.com>
2018-12-20 01:57:31 -0300

    Inicio de chequeo

---
+++ chequeo_valores.py
@@ -0,0 +1,7 @@
+#Probando valores
+
+banned_users = ['andrew', 'carolina', 'david']
+user = 'marie'
+
+if user not in banned_users:
+    print(user.title() + ", puedes postear una respuesta si quieres.")
