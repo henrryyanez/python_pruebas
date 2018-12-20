d437ca18c68da9555ce9ddbd6f47a8c10ebe60dc
hy <henrryyanez@gmail.com>
2018-12-20 02:00:53 -0300

    Le agrego un input para interacción

--- chequeo_valores.py
+++ chequeo_valores.py
@@ -1,7 +1,7 @@
 #Probando valores
 
 banned_users = ['andrew', 'carolina', 'david']
-user = 'marie'
+user = input('')
 
 if user not in banned_users:
     print(user.title() + ", puedes postear una respuesta si quieres.")
