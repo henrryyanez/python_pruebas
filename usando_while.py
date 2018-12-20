e246c9dbcd04cc1fb8e44d94b9cae93f51c433c2
hy <henrryyanez@gmail.com>
2018-12-19 23:37:07 -0300

    Update usando_while.py

--- usando_while.py
+++ usando_while.py
@@ -4,3 +4,11 @@ while numero <= 10:
     suma = numero + suma
     numero = numero + 1
 print("La suma es " + str(suma))
+
+###Condicion inicial
+suma, numero = 0, 6
+
+while numero <= 10:
+    suma = numero + suma
+    numero = numero + 1
+print("La suma es " + str(suma))
