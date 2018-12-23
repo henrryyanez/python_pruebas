07b94cdb6d2e8147c0c01672abadfbbe52f2cac6
hy <henrryyanez@gmail.com>
2018-12-23 00:06:58 -0300

    primera calculadora

---
+++ calculando.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+impuesto = 12.5 / 100
+precio = 100.50
+precio * impuesto
+#12.5625
+
+precio + _   # el caracter _ guarda el ultimo resultado arrojado en una variable
+#113.0625
+
+round(_, 2)
+#113.06
