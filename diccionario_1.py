70b5106bca3c42645623a942a5e171493b75e2d3
hy <henrryyanez@gmail.com>
2018-12-24 01:58:31 -0300

    Update diccionario_1.py

--- diccionario_1.py
+++ diccionario_1.py
@@ -1,5 +1,6 @@
 #Inicio del discionario
 
-alien_0 = {'color': 'green', 'points': 5}
+alien_0 = {'color': 'green', 'points': 5, 'age': 33}
 print(alien_0['color'])
 print(alien_0['points'])
+print(alien_0['age'])
