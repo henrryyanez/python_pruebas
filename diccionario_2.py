a175363625f7dea0789c017c8b2cc85864ae0010
hy <henrryyanez@gmail.com>
2018-12-24 03:30:52 -0300

    Multiple diccionario con for

--- diccionario_2.py
+++ diccionario_2.py
@@ -8,3 +8,25 @@ user_0 = {
 for key, value in user_0.items():
     print("\nKey: " + key)
     print("Value: " + value)
+
+print("\nSegunda prueba\n")
+   
+   users = {
+'aeinstein': {
+'first': 'albert',
+'last': 'einstein',
+'location': 'princeton',
+},
+'mcurie': {
+'first': 'marie',
+'last': 'curie',
+'location': 'paris',
+},
+}
+
+for username, user_info in users.items():
+    print("\nUsername: " + username)
+    full_name = user_info['first'] + " " + user_info['last']
+    location = user_info['location']
+    print("\tFull name: " + full_name.title())
+    print("\tLocation: " + location.title())
