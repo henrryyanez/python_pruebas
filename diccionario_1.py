7041813d1d54001c38c48496cc397775091c589c
hy <henrryyanez@gmail.com>
2018-12-24 02:06:39 -0300

    Update diccionario_1.py

--- diccionario_1.py
+++ diccionario_1.py
@@ -4,3 +4,6 @@ alien_0 = {'color': 'green', 'points': 5, 'age': 33}
 print(alien_0['color'])
 print(alien_0['points'])
 print(alien_0['age'])
+
+new_points = alien_0['points']
+print("You just earned " + str(new_points) + " points!")
