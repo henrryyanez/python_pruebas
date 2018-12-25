6740b4d6e4654644088f58cec469fb2b3fca345d
hy <henrryyanez@gmail.com>
2018-12-25 18:29:39 -0300

    Segunda prueba

--- input_while_loops.py
+++ input_while_loops.py
@@ -6,3 +6,12 @@ if height >= 36:
  print("\nYou're tall enough to ride!")
 else:
  print("\nYou'll be able to ride when you're a little older.")
+
+ print("\n Practica 2 \n"
+
+number = input("Enter a number, and I'll tell you if it's even or odd: ")
+number = int(number)
+if number % 2 == 0:
+ print("\nThe number " + str(number) + " is even.")
+else:
+ print("\nThe number " + str(number) + " is odd.")
