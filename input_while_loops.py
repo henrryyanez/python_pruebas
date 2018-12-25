73c8605d8f33b965db8314b9ae3e707b49388c3f
hy <henrryyanez@gmail.com>
2018-12-25 18:26:12 -0300

    Primer ejemplo funcionando

---
+++ input_while_loops.py
@@ -0,0 +1,8 @@
+#Usando while con loops
+
+height = input("Cual es tu talla, en pulgadas? ")
+height = int(height)
+if height >= 36:
+ print("\nYou're tall enough to ride!")
+else:
+ print("\nYou'll be able to ride when you're a little older.")
